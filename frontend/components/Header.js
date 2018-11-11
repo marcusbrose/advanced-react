d004deb81a4bb130299ba00d31a86317035965ac
Marcus Brose <contact@marcusbrose.com>
2018-11-11 14:52:27 +0100

    search with downshift + debounce

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 import NProgress from 'nprogress'
 import Nav from './Nav'
 import Cart from './Cart'
+import Search from './Search'
 
 const Logo = styled.h1`
   font-size: 4rem;
@@ -64,9 +65,7 @@ const Header = props => (
       <Nav />
     </div>
     <div className="sub-bar">
-      <p>
-        Search
-      </p>
+      <Search />
     </div>
     <Cart />
   </StyledHeader>
